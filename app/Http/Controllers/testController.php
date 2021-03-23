c4e9558ceb8f91a68b387fa4f1b58a8f44840f1f
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-03-23 10:09:44 +0600

    test

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -4,50 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Stevebauman\Location\Facades\Location;
 
 class testController extends Controller
 {
     public function userInfo(Request $request)
     {
 
-        // if (DB::table('user_infos')->where([
-        //     'website' => $request->website,
-        //     'ip' => $request->ip
-        // ])->exists()) {
-        //     $data = DB::table('user_infos')->where([
-        //         'website' => $request->website,
-        //         'ip' => $request->ip
-        //     ])->get();
+        if (DB::table('user_infos')->where([
+            'website' => $request->website,
+            'ip' => $request->ip
+        ])->exists()) {
+            $data = DB::table('user_infos')->where([
+                'website' => $request->website,
+                'ip' => $request->ip
+            ])->get();
 
-        //     $visited = $data[0]->visited;
+            $visited = $data[0]->visited;
 
-        //     DB::table('user_infos')->where([
-        //         'website' => $request->website,
-        //         'ip' => $request->ip
-        //     ])->update([
-        //         'visited' => $visited + 1
-        //     ]);
-        // } else {
-        //     DB::table('user_infos')->insert(
-        //         [
-        //             'ip' => $request->ip,
-        //             'continent_name' => $request->continent_name,
-        //             'calling_code' => $request->calling_code,
-        //             'city' => $request->city,
-        //             'country_name' => $request->country_name,
-        //             'district' => $request->district,
-        //             'zipcode' => $request->zipcode,
-        //             'isp' => $request->isp,
-        //             'website' => $request->website,
-        //             "visited" => 1,
-        //             "created_at" =>  \Carbon\Carbon::now(),
-        //             "updated_at" => \Carbon\Carbon::now(),
-        //         ]
-        //     );
-        // }
-        $locationData = Location::get();;
-        return response()->json(["success" => $locationData]);
+            DB::table('user_infos')->where([
+                'website' => $request->website,
+                'ip' => $request->ip
+            ])->update([
+                'visited' => $visited + 1
+            ]);
+        } else {
+            DB::table('user_infos')->insert(
+                [
+                    'ip' => $request->ip,
+                    'continent_name' => $request->continent_name,
+                    'calling_code' => $request->calling_code,
+                    'city' => $request->city,
+                    'country_name' => $request->country_name,
+                    'district' => $request->district,
+                    'zipcode' => $request->zipcode,
+                    'isp' => $request->isp,
+                    'website' => $request->website,
+                    "visited" => 1,
+                    "created_at" =>  \Carbon\Carbon::now(),
+                    "updated_at" => \Carbon\Carbon::now(),
+                ]
+            );
+        }
+        return response()->json(["success" => 'success']);
     }
     public function getUserInfo()
     {

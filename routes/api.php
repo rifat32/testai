d8e05d5bab6f72427dae0bc7074d2e4c8179eada
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-05-09 10:25:11 +0600

    test

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,4 @@ Route::get('user', [testController::class, 'getUserInfo']);
 
 Route::post('message', [testController::class, 'userMessage']);
 Route::get('message', [testController::class, 'getUserMessage']);
-Route::get('test', [testController::class, 'test']);
+Route::post('test', [testController::class, 'test']);

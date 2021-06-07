ba846a6b04fa7f9aae32050c303803bd95656c44
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-06-08 00:39:41 +0600

    some fix

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -127,18 +127,16 @@ class testController extends Controller
         $tableQuery = $table->where([
             'text' => $inputText
             ]);
+
+
         if($tableQuery->exists()){
             $text = $tableQuery->first();
-            $previous_count   = $text->count;
             $tableQuery->update([
-            'count' => $previous_count + 1
+            'count' => $text->count + 1
             ]);
-            $updated_text = $tableQuery->first();
-            $updated_count = $updated_text->count;
             return response()->json([
            'status' => 200,
-           'previousCount' => $previous_count,
-           'updatedCount' => $updated_count
+           'count' =>$text->count + 1
                 ]);
 
 
@@ -149,10 +147,10 @@ class testController extends Controller
                 'count' => 1
             ]);
             return response()->json([
-                'status' => 201
+                'status' => 201,
+                'count' => 1
                      ]);
         }
-
 
     }
 }
